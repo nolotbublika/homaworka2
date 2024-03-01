a7b5613e66f942d67393c7a7f915e2ce55bbd2e9
nolotbublika <161640172+nolotbublika@users.noreply.github.com>
2024-03-01 22:31:26 +0300

    Add files via upload

---
+++ def.py
@@ -0,0 +1,15 @@
+def print_params():
+    print("forvard wheels "*2)
+
+
+print_params()
+print_params()
+print_params()
+
+
+def print_params(arg):
+    print("day ", arg)
+
+
+print_params('light')
+print_params('by day')
