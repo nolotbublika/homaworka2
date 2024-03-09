e65e2d3850fea564ad62d3916ae218563b0974fb
nolotbublika <161640172+nolotbublika@users.noreply.github.com>
2024-03-09 10:23:17 +0300

    Update def.py
    первая функция удалил
    вторая функция изменено

--- def.py
+++ def.py
@@ -1,15 +1,5 @@
-def print_params():
-    print("forvard wheels "*2)
-
-
-print_params()
-print_params()
-print_params()
-
-
 def print_params(arg):
     print("day ", arg)
-
+    print ("nigh",arg)
 
 print_params('light')
-print_params('by day')
